59a06872048cfce4bc1b3f41d0bd56adcd9a9c02
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-05 16:30:15 +0100

    build: Replace pushd with cd
    * For consistency
    
    Change-Id: I96711e3be7b73ebadd589f460e5c5e90d000a4f7
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -650,13 +650,13 @@ function lunch()
     then
         # if we can't find the product, try to grab it from our github
         T=$(gettop)
-        pushd $T > /dev/null
+        cd $T > /dev/null
         vendor/extras/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
-        pushd $T > /dev/null
+        cd $T > /dev/null
         vendor/extras/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
