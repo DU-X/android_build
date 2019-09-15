ce4c0bbb70546d13653203fad6717a9ed4990e9f
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-09-15 10:01:04 +0200

    Revert "Include pathmap"
    This reverts commit 12b7d45c16d6b64e5d9d3e8e498bf487f4933398.

--- core/config.mk
+++ core/config.mk
@@ -994,6 +994,9 @@ $(foreach device,$(call to-upper,$(BOARD_SUPER_PARTITION_BLOCK_DEVICES)), \
 
 endif # PRODUCT_BUILD_SUPER_PARTITION
 
+# Rules for QCOM targets
+include vendor/extras/build/core/qcom_target.mk
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
@@ -1181,9 +1184,3 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 include $(BUILD_SYSTEM)/dumpvar.mk
-
-# QCOM targets and pathmap
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include $(TOPDIR)vendor/extras/build/core/pathmap.mk
-include $(TOPDIR)vendor/extras/build/core/qcom_target.mk
-endif
