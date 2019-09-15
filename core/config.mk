erfanoabdi/build_make

config: Include custom boardconfig a bit later
- We're trying to export MAKE_PREBUILT to soong
- Let's include it right before soong_config.mk is included

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

core/config.mk
@@ -234,11 +234,6 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # be device and hardware independent.
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
--include vendor/extra/BoardConfigExtra.mk
-ifneq ($(POTATO_BUILD),)
-include vendor/potato/config/BoardConfigPotato.mk
-endif
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1185,6 +1180,11 @@ dont_bother_goals := out \
     recoveryimage-nodeps \
     vbmetaimage-nodeps \
     product-graph dump-products
+
+-include vendor/extra/BoardConfigExtra.mk
+ifneq ($(POTATO_BUILD),)
+include vendor/potato/config/BoardConfigPotato.mk
+endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk

DU-X/android_build

Change-Id: I93308c35f5d3112da51fa944b7ed193ccff8e3e8

core/config.mk
@@ -230,10 +230,6 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(DU_BUILD),)
-include vendor/extras/config/BoardConfigDu.mk
-endif
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1177,11 +1173,12 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
-#ifneq ($(DU_BUILD),)
+ifneq ($(DU_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 #$(eval include device/du/sepolicy/common/sepolicy.mk)
-#endif
+include vendor/extras/config/BoardConfigDu.mk
+endif
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk